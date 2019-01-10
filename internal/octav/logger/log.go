6840021bb742986b2255f004d042c79c66d4c3b3
ShellCode33 <shellcode33@protonmail.ch>
2019-01-10 16:40:33 +0100

    Add colors and date

--- internal/octav/logger/log.go
+++ internal/octav/logger/log.go
@@ -2,30 +2,51 @@ package logger
 
 import (
 	"fmt"
+	"time"
 )
 
-// TODO : colors, file output
+const (
+	InfoColor    = "\033[1;34m"
+	WarningColor = "\033[1;33m"
+	ErrorColor   = "\033[1;31m"
+	DebugColor   = "\033[0;36m"
+	ResetColor   = "\033[0m"
+)
+
+// TODO : file output, print time only in file
 
 func Debug(msg string) {
-	fmt.Println("[@] " + msg)
+	fmt.Printf("%v[DEBUG] [%v] %v%v\n", DebugColor, getCurrentTime(), msg, ResetColor)
 }
 
 func Info(msg string) {
-	if verboseLevel >= VERBOSE_INFO {
-		fmt.Println("[+] " + msg)
+	if verboseLevel == VERBOSE_DEBUG {
+		fmt.Printf("%v[INFO] [%v] %v%v\n", InfoColor, getCurrentTime(), msg, ResetColor)
+	} else if verboseLevel >= VERBOSE_INFO {
+		fmt.Printf("%v[INFO] %v%v\n", InfoColor, msg, ResetColor)
 	}
 }
 
 func Warning(msg string) {
-	if verboseLevel >= VERBOSE_WARNING {
-		fmt.Println("[!] " + msg)
+	if verboseLevel == VERBOSE_DEBUG {
+		fmt.Printf("%v[WARNING] [%v] %v%v\n", WarningColor, getCurrentTime(), msg, ResetColor)
+	} else if verboseLevel >= VERBOSE_WARNING {
+		fmt.Printf("%v[WARNING] %v%v\n", WarningColor, msg, ResetColor)
 	}
 }
 
 func Error(msg string) {
-	fmt.Println("[x] " + msg)
+	if verboseLevel == VERBOSE_DEBUG {
+		fmt.Printf("%v[ERROR] [%v] %v%v\n", ErrorColor, getCurrentTime(), msg, ResetColor)
+	} else {
+		fmt.Printf("%v[ERROR] %v%v\n", ErrorColor, msg, ResetColor)
+	}
 }
 
 func Fatal(msg string) {
-	panic("[FATAL] " + msg)
+	panic(fmt.Sprintf("%v[FATAL] [%v] %v%v", ErrorColor, getCurrentTime(), msg, ResetColor))
+}
+
+func getCurrentTime() string {
+	return time.Now().Format("15:04:05")
 }
