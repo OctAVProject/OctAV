e0defc1c278ee8b580752962cf3eff0128ab017a
ShellCode33 <shellcode33@protonmail.ch>
2019-05-22 13:49:37 +0200

    Small improvements + dynamic analysis working (cuckoo => syscall extraction)

--- internal/octav/core/analyzer.go
+++ internal/octav/core/analyzer.go
@@ -2,13 +2,19 @@ package core
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/OctAVProject/OctAV/internal/octav/core/analysis"
+	"github.com/OctAVProject/OctAV/internal/octav/core/analysis/dynamic"
 	"github.com/OctAVProject/OctAV/internal/octav/core/analysis/static"
 	"github.com/OctAVProject/OctAV/internal/octav/logger"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func Analyse(filename string) error {
@@ -22,28 +28,46 @@ func Analyse(filename string) error {
 	logger.Info("Analysing " + filename)
 	logger.Debug(exe.String())
 
-	threatScore, err := staticAnalysis(exe)
+	start := time.Now()
+	staticThreatScore, err := staticAnalysis(exe)
 
 	if err != nil {
 		return errors.New("Not able to perform static analysis : " + err.Error())
 	}
 
-	fmt.Println("Score: ", threatScore)
+	elapsed := time.Now().Sub(start)
+	logger.Info(fmt.Sprintf("Static Analysis done in %v", elapsed))
 
-	// TODO : If static analysis is sure the binary is a threat, skip dynamic analysis
-	/*
-		threatScore, err = dynamicAnalysis(exe)
+	fmt.Println("Static score: ", staticThreatScore)
 
-		if err != nil {
-			return errors.New("Not able to perform dynamic analysis : " + err.Error())
-		}
-
-		fmt.Println("Score: ", threatScore)
-	*/
-
-	if threatScore >= 100 {
+	if staticThreatScore >= 100 {
 		logger.Danger("Threat score >= 100")
 		malwareDetected(exe)
+		return nil
+	}
+
+	start = time.Now()
+	dynamicThreatScore, err := dynamicAnalysis(exe)
+
+	if err != nil {
+		return errors.New("Not able to perform dynamic analysis : " + err.Error())
+	}
+
+	elapsed = time.Now().Sub(start)
+	logger.Info(fmt.Sprintf("Dynamic Analysis done in %v", elapsed))
+
+	fmt.Println("Dynamic score: ", dynamicThreatScore)
+
+	if dynamicThreatScore >= 100 {
+		logger.Danger("Threat score >= 100")
+		malwareDetected(exe)
+		return nil
+	}
+
+	if staticThreatScore+dynamicThreatScore >= 170 {
+		logger.Danger("Additional score >= 170")
+		malwareDetected(exe)
+		return nil
 	}
 
 	return nil
@@ -72,7 +96,6 @@ func staticAnalysis(exe *analysis.Executable) (uint, error) {
 	}
 
 	if hashIsKnown {
-		malwareDetected(exe)
 		return 100, nil
 	}
 
@@ -110,10 +133,9 @@ func staticAnalysis(exe *analysis.Executable) (uint, error) {
 		}
 	}
 
-	if ssDeepDistance > 80 {
-		logger.Danger(fmt.Sprintf("SSDeep distance is higher than 80 (%v), considered a malware.", ssDeepDistance))
-		malwareDetected(exe)
-		return 0, nil
+	if ssDeepDistance > 90 {
+		logger.Danger("SSDeep distance is higher than 90")
+		score += 80
 	}
 
 	logger.Info("Looking for matching YARA rules")
@@ -153,8 +175,8 @@ func staticAnalysis(exe *analysis.Executable) (uint, error) {
 					score += 20
 
 				default:
-					malwareDetected(exe)
-					return 0, nil
+					logger.Danger(fmt.Sprintf("Program matched YARA rule '%v' meaning it's a malware", match.Rule))
+					return 100, nil
 				}
 
 			case "packer":
@@ -184,7 +206,125 @@ func staticAnalysis(exe *analysis.Executable) (uint, error) {
 }
 
 func dynamicAnalysis(exe *analysis.Executable) (uint, error) {
+
 	fmt.Println("\n_____DYNAMIC_ANALYSIS_____")
+
+	var requestBody bytes.Buffer
+
+	writer := multipart.NewWriter(&requestBody)
+
+	fieldWriter, err := writer.CreateFormFile("file", exe.Filename)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = fieldWriter.Write(exe.Content)
+	if err != nil {
+		return 0, err
+	}
+
+	fieldWriter, err = writer.CreateFormField("unique")
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = fieldWriter.Write([]byte("true"))
+	if err != nil {
+		return 0, err
+	}
+
+	writer.Close()
+
+	request, err := http.NewRequest("POST", "http://localhost:8090/tasks/create/file", &requestBody)
+	if err != nil {
+		return 0, err
+	}
+
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var jsonResponse map[string]interface{}
+	var taskID int
+
+	if resp.StatusCode == http.StatusOK {
+		logger.Debug("Binary has been submitted to the Cuckoo Sandbox")
+
+		if json.NewDecoder(resp.Body).Decode(&jsonResponse) != nil {
+			return 0, err
+		}
+
+		taskID = int(jsonResponse["task_id"].(float64))
+
+	} else if resp.StatusCode == http.StatusBadRequest {
+		logger.Debug("Binary has already been analysed by the Cuckoo Sandbox")
+
+		resp, err = http.Get("http://localhost:8090/files/view/sha256/" + exe.SHA256)
+		if err != nil {
+			return 0, err
+		}
+
+		if json.NewDecoder(resp.Body).Decode(&jsonResponse) != nil {
+			return 0, nil
+		}
+
+		sample := jsonResponse["sample"].(map[string]interface{})
+		logger.Debug(fmt.Sprintf("sample: %v", sample))
+		taskID = int(sample["id"].(float64))
+		resp.Body.Close()
+	}
+
+	logger.Debug(fmt.Sprintf("Cuckoo Task ID: %v", taskID))
+
+	for {
+		resp, err = http.Get(fmt.Sprintf("http://localhost:8090/tasks/report/%v", taskID))
+		if err != nil {
+			return 0, err
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			break
+		}
+
+		resp.Body.Close()
+
+		logger.Debug("Waiting for the report...")
+		time.Sleep(2 * time.Second)
+	}
+
+	logger.Debug(string("Report ready !"))
+
+	if json.NewDecoder(resp.Body).Decode(&jsonResponse) != nil {
+		return 0, nil
+	}
+
+	resp.Body.Close()
+
+	behavior := jsonResponse["behavior"].(map[string]interface{})
+	processes := behavior["processes"].([]interface{})
+
+	for _, process := range processes {
+		calls := process.(map[string]interface{})["calls"].([]interface{})
+		syscalls := make([]int, 1)
+
+		for _, call := range calls {
+			syscallName := call.(map[string]interface{})["api"].(string)
+			syscalls = append(syscalls, dynamic.Syscalls[syscallName])
+		}
+
+		modelScore, err := dynamic.ApplyModel(syscalls)
+
+		if modelScore > 60 {
+			return 100, err
+		}
+	}
+
 	return 0, nil
 }
 
